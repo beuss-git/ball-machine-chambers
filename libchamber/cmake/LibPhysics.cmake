8761c357da4b373caeeb491cef45915af87125db
Benjamin Bjerken <beuss.code@gmail.com>
2024-10-19 13:59:53 +0200

    Clean up and format CMake files

--- libchamber/cmake/LibPhysics.cmake
+++ libchamber/cmake/LibPhysics.cmake
@@ -1,19 +1,19 @@
 include(FetchContent)
 
-set(libphysics_path "${CMAKE_CURRENT_SOURCE_DIR}/libs/libphysics.a")
+set(LIBPHYSICS_PATH "${CMAKE_CURRENT_SOURCE_DIR}/libs/libphysics.a")
 
-if(NOT EXISTS ${libphysics_path})
+if(NOT EXISTS ${LIBPHYSICS_PATH})
   FetchContent_Declare(
     libphysics
     URL https://sphaerophoria.dev/libphysics.a
     DOWNLOAD_DIR ${CMAKE_CURRENT_SOURCE_DIR}/libs
     DOWNLOAD_NO_EXTRACT TRUE
-    )
+  )
 
   FetchContent_MakeAvailable(libphysics)
 endif()
 
 add_library(physics STATIC IMPORTED GLOBAL)
 set_target_properties(physics PROPERTIES
-  IMPORTED_LOCATION ${libphysics_path}
-  )
+  IMPORTED_LOCATION ${LIBPHYSICS_PATH}
+)
